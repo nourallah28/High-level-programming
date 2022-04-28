99e5033460321fc01a7720c7790eda7bbfe4387e
Nourallah <91788381+nourallah28@users.noreply.github.com>
2022-04-28 23:38:53 +0200

    Update ch8_drill_2.cpp

--- ch8_drill_2.cpp
+++ ch8_drill_2.cpp
@@ -11,10 +11,9 @@ void swap_v(int a, int b)
 
 
 void swap_r(int& a, int& b)
-	//since it is a refrence int type value 
-	//it can't be compiled to a value of type int
-	//if the fuction is a double and the value of x and y are also double
-	//it would compile
+	//since it is a refrence which means it refers to the x,y and isn't a copy
+	//the x and y will swap 
+	//but when putting numbers it won't swap
 {
 	int temp;
 	temp= a;
@@ -37,6 +36,8 @@ void swap_r(int& a, int& b)
 
 int main()
 {
+//in here it didn't swap because this is just a copy
+//however it swapped in the function itself
 	int x=7;
 	int y=9;
 	swap_v(x,y);
@@ -57,8 +58,5 @@ int main()
 	swap_v(7.7,9.9);
 	cout <<"dx= "<<dx<<"\n";
 	cout <<"dy= "<<dy<<"\n";
-	
-	
-	
 	
 }
