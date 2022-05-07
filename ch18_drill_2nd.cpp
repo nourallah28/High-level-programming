3c70dee572f333c790c9d6060390426d4f66c8b5
Nourallah <91788381+nourallah28@users.noreply.github.com>
2022-05-07 09:24:17 +0200

    Create ch18_drill_2nd.cpp

---
+++ ch18_drill_2nd.cpp
@@ -0,0 +1,33 @@
+#include "std_lib_facilities.h"
+
+//q.1
+vector<int> gv {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+//q.2
+void f(vector<int> v)
+{
+    vector<int> lv(10);
+    lv = v;
+    for (auto& a : lv)
+        cout << a << "\t";
+    cout << "\n";
+
+    vector<int> lv2 = v;
+    for (auto& a : lv2)
+        cout << a << "\t";
+    cout << "\n";
+}
+
+int fac(int n) { return n > 1 ? n * fac(n - 1) : 1; }
+
+//q.4
+int main()
+{
+    f(gv);
+
+    vector<int> vv(10);
+    for (int i = 0; i < vv.size(); ++i)
+        vv[i] = fac(i + 1);
+    f(vv);
+}
+
